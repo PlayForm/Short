7b41b61c8453d0eebdb2843f4ba75f563c752755
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-18 12:47:51 +0300

    Cleanup

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
-import{deepmerge as s}from"deepmerge-ts";import n from"./options/index.js";var p=(r={})=>{var e;for(const o in r)Object.prototype.hasOwnProperty.call(r,o)&&r[o]===!0&&(r[o]=n()[o]);const t=s(n(),r);return t.url=(e=t.url)!=null&&e.endsWith("/")?t.url:`${t.url}/`,{name:"astro-short-urls",hooks:{"astro:build:done":async()=>{}}}};export{p as default};
+import { deepmerge as s } from "deepmerge-ts";
+import n from "./options/index.js";
+var p=(r={})=>{var e;for(const o in r)Object.prototype.hasOwnProperty.call(r,o)&&r[o]===!0&&(r[o]=n()[o]);const t=s(n(),r);return t.url=(e=t.url)!=null&&e.endsWith("/")?t.url:`${t.url}/`,{name:"astro-short-urls",hooks:{"astro:build:done":async()=>{}}}};export { p as default };
+
