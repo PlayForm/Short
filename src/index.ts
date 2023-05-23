3fcd3378c356ca7537d2bf7d08330002cd478abd
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-23 21:34:49 +0300

    squash!

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import type { AstroIntegration } from "astro";
+import { deepmerge } from "deepmerge-ts";
+import defaultOptions, { Options } from "./options/index.js";
+
+export default (options: Options = {}): AstroIntegration => {
+	for (const option in options) {
+		if (
+			Object.prototype.hasOwnProperty.call(options, option) &&
+			options[option] === true
+		) {
+			options[option] = defaultOptions()[option];
+		}
+	}
+
+	const _options = deepmerge(defaultOptions(), options);
+
+	_options.url = _options.url?.endsWith("/")
+		? _options.url
+		: `${_options.url}/`;
+
+	return {
+		name: "astro-short-urls",
+		hooks: {
+			"astro:build:done": async () => {},
+		},
+	};
+};
