ed3bed240fb02d379730b8a53e14d6bfdcb9f41d
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-01 22:43:11 +0300

    squash!

--- src/Index.ts
+++ src/Index.ts
@@ -1,6 +1,6 @@
 import type { AstroIntegration } from "astro";
 import { deepmerge } from "deepmerge-ts";
-import defaultOptions, { Options } from "./options/index.js";
+import defaultOptions, { Options } from "./options/Index.js";
 
 export default (options: Options = {}): AstroIntegration => {
 	for (const option in options) {
