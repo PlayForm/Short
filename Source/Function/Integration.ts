a80be9c69e212180b12ecc35e13261a3332c6a85
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-01 14:08:20 +0300

    squash!

--- Source/Function/Integration.ts
+++ Source/Function/Integration.ts
@@ -27,5 +27,5 @@ import type Option from "../Interface/Option.js";
 import type { AstroIntegration } from "astro";
 
 export const { default: Merge } = await import(
-	"typescript-esbuild/Target/Function/Merge.js"
+	"@playform/build/Target/Function/Merge.js"
 );
