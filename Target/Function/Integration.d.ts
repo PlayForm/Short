e6732d690d1da32c2cc40b925748e6449e807720
Nikola Hristov <nikola@playform.cloud>
2024-05-07 16:14:05 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -2,11 +2,10 @@
  * @module Integration
  *
  */
-declare const _default: (_Option?: Option) => AstroIntegration;
+declare const _default: Interface;
 export default _default;
+import type Interface from "../Interface/Integration.js";
 export declare const Default: {
     Logger: number;
 };
-import type Option from "../Interface/Option.js";
-import type { AstroIntegration } from "astro";
 export declare const Merge: import("@playform/build/Target/Interface/Merge.js").default<import("@playform/build/Target/Interface/Merge.js").Generic>;
--- Target/Interface/Option.d.ts
+++ Target/Interface/Option.d.ts
@@ -2,6 +2,6 @@
  * @module Option
  *
  */
-export default interface Type {
+export default interface Interface {
     Logger?: number;
 }
