d83598b8f457100c57cd9eb13d08a08135c76fb0
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-29 04:04:00 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -6,14 +6,4 @@ declare const _default: Interface;
 export default _default;
 import type Interface from "../Interface/Integration.js";
 export declare const Default: import("../Interface/Option.js").default;
-export declare const Merge: <Ts extends readonly unknown[]>(...objects: Ts) => import("deepmerge-ts").DeepMergeHKT<Ts, Readonly<{
-    DeepMergeRecordsURI: "DeepMergeRecordsDefaultURI";
-    DeepMergeArraysURI: "DeepMergeArraysDefaultURI";
-    DeepMergeSetsURI: "DeepMergeSetsDefaultURI";
-    DeepMergeMapsURI: "DeepMergeMapsDefaultURI";
-    DeepMergeOthersURI: "DeepMergeLeafURI";
-    DeepMergeFilterValuesURI: "DeepMergeFilterValuesDefaultURI";
-}>, Readonly<{
-    key: PropertyKey;
-    parents: readonly Readonly<Record<PropertyKey, unknown>>[];
-}>>;
+export declare const Merge: <Ts extends readonly unknown[]>(...objects: Ts) => import("deepmerge-ts").DeepMergeHKT<Ts, import("deepmerge-ts").GetDeepMergeFunctionsURIs<{}>, import("deepmerge-ts").DeepMergeBuiltInMetaData>;
